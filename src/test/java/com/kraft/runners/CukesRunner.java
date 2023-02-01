be59b0f85fe7ea8d8097c0956296ce44b425d75b
alparslan <alparslanztk@gmail.com>
2023-02-01 18:49:45 +0300

    second commit added

--- src/test/java/com/kraft/runners/CukesRunner.java
+++ src/test/java/com/kraft/runners/CukesRunner.java
@@ -9,10 +9,9 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/kraft/step_definitions",
         dryRun = false,
-        tags = "@wip"
+        tags = "@register"
 
 )
-
 
 public class CukesRunner {
 }
